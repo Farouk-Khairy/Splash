b117d3ca9725d7ff41f4ed305b3e65cf1a583cb9
Farouk Khairy <faroukkhairy1998@gmail.com>
2022-03-16 00:42:25 +0200

    final result of project

--- js/content.js
+++
@@ -1,5 +0,0 @@
-let gear = document.querySelector(".gear");
-let sideBar = document.querySelector("aside");
-gear.addEventListener("click", () => {
-  sideBar.classList.toggle("show");
-});
--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,7 @@
-let myList = document.querySelector("nav .list"),
-  toggle = document.querySelector("nav .toggle");
-toggle.addEventListener("click", () => {
-  myList.classList.toggle("active");
+const nav = document.querySelector("nav");
+const toggleBtn = document.querySelector(".toggle-bar");
+
+toggleBtn.addEventListener("click", () => {
+  toggleBtn.classList.toggle("transform");
+  nav.classList.toggle("list-active");
 });
